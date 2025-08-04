b50d6c59154756e7eb363d3b3da33d13109d6307
movinggun0219 <daugun@naver.com>
2025-08-04 15:37:42 +0900

    08/04

--- Assets/_Unity Essentials/Scripts/Collectible.cs
+++ Assets/_Unity Essentials/Scripts/Collectible.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 public class Collectible : MonoBehaviour
 {
     public float rotationSpeed;
+    public GameObject OncollectEffect;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(0, rotationSpeed, 0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            Instantiate(OncollectEffect, transform.position, transform.rotation);
+        }
+        
     }
 }
